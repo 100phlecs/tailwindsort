d823656f1ffe26519f21fe4e458de482534121ee
100phlecs <hello@based.town>
2022-08-19 23:19:08 -0400

    extract rest of classes

--- src/index.js
+++ src/index.js
@@ -77,24 +77,31 @@ export async function format(contents, relativeConfigPath) {
     const baseClass = currentClass.value
     const beforeDeets = twcontext.candidateRuleMap.get(currentClass.value)
 
-    const deets = beforeDeets[0][0]     
-    let potentialClasses = [];
-    if ("values" in deets.options) {
-      const classValues = Object.keys(deets.options.values)
-      const supportsNegative = deets.options.supportsNegativeValues
-      
-      potentialClasses = classValues.map(value => value === "DEFAULT" ? baseClass : `${baseClass}-${value}`)
-      if (supportsNegative) {
-        potentialClasses = potentialClasses.map(c => c.includes('auto') ? c : [`-${c}`, c]).flat()
+    for (let eachDeet of beforeDeets) {
+      const deets = eachDeet[0]
+      let potentialClasses = []
+      if ("values" in deets.options) {
+        const classValues = Object.keys(deets.options.values)
+        const supportsNegative = deets.options.supportsNegativeValues
+        
+        potentialClasses = classValues.map(value => value === "DEFAULT" ? baseClass : `${baseClass}-${value}`)
+        if (supportsNegative) {
+          potentialClasses = potentialClasses.map(c => c.includes('auto') ? c : [`-${c}`, c]).flat()
+        }
+      } else {
+        potentialClasses.push(baseClass)
       }
-    } else {
-      potentialClasses.push(baseClass)
+
+      allClasses.push(potentialClasses)
+      allClasses = allClasses.flat()
     }
-    allClasses.push(potentialClasses)
-    allClasses = allClasses.flat()
 
     currentClass = classIterator.next()
   }
+
+  allClasses = allClasses.filter(function onlyUnique(value, index, self) {
+                                    return self.indexOf(value) === index;
+                                  });
 
 
   twcontext.variantMap.forEach( (applyfunc, variant) => allVariants.push([variant, applyfunc[0][0]]))
@@ -103,14 +110,14 @@ export async function format(contents, relativeConfigPath) {
       if (a === null) return -1
       if (z === null) return 1
       return bigSign(a - z)
-    }).map(([className, bigInt]) => `${className}: ${bigInt == null ? "null" : bigInt.toString()}`).join("\n")
+    }).map(([className]) => className).join("\n") //map(([className, bigInt]) => `${className}: ${bigInt == null ? "null" : bigInt.toString()}`).join("\n")
 
   let orderedClasses = twcontext.getClassOrder(allClasses).sort(([, a], [, z]) => {
       if (a === z) return 0
       if (a === null) return -1
       if (z === null) return 1
       return bigSign(a - z)
-    }).map(([className, bigInt]) => `${className}: ${bigInt == null ? "null" : bigInt.toString()}`).join("\n")
+    }).map(([className]) => className).join("\n") //map(([className, bigInt]) => `${className}: ${bigInt == null ? "null" : bigInt.toString()}`).join("\n")
 
   fs.writeFileSync('out/classes.txt', orderedClasses)
   fs.writeFileSync('out/variants.txt', allVariants)
